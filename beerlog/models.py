89b834d0feed727a93cbf8261541c11285e09987
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2022-04-27 02:41:49 +0000

    mapeando objeto

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
 
 
-@dataclass
-class Beer:
+class Beer(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
     name: str
     style: str
     flavor: int
